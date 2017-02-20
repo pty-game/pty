c035965bcfa10142c479fc32925ffb779348b2dd
Vadym Bondarenko <danxil@list.ru>
2017-02-20 22:09:05 +0100

    Refactoring. Next: crontab tests

--- server/crontab/applications-connect.js
+++ server/crontab/applications-connect.js
@@ -4,6 +4,129 @@ import GameCtrl from '../controllers/game';
 const gameCtrl = new GameCtrl();
 
 export default class ApplicationConnect {
+  async findOpponentForPlayer({ gameApplication, gameApplications }) {
+    return gameApplications.find((gameApplicationSub) => {
+      return !gameApplicationSub.is_estimator &&
+        gameApplication.id !== gameApplicationSub.id;
+    });
+  }
+
+  async iterationForEstimator({ gameApplication, db }) {
+    const gameWithMinEstimators = await gameCtrl.findWithMinEstimators({
+      finderUserId: gameApplication.userId,
+      db,
+    });
+
+    if (!gameWithMinEstimators) {
+      return false;
+    }
+
+    await db.GameUser.create({
+      userId: gameApplication.user,
+      gameId: gameWithMinEstimators.id,
+      is_estimator: true,
+    });
+
+    // TODO websocket
+    // GameApplication.message(
+    //   gameApplication.id,
+    //   wsResponses.message('gameFound', {gameId: game.id})
+    // )
+
+    await gameApplication.destroy();
+
+    return true;
+  }
+
+  async iterationForPlayer({ gameApplication, gameApplications, index, db }) {
+    /* eslint-disable no-param-reassign */
+    const gameApplicationSub = this.findOpponentForPlayer({ gameApplication, gameApplications });
+
+    if (
+      !gameApplicationSub &&
+      gameApplication.residueTime > gameConfig.RESIDUE_TIME_FOR_PAINTER_BOTS
+    ) {
+      return this.gameApplicationIteration({ gameApplications, index: index + 1, db });
+    }
+
+    const game = await gameCtrl.create();
+    const gameId = game.id;
+
+    if (
+      !gameApplicationSub &&
+      gameApplication.residueTime <= gameConfig.RESIDUE_TIME_FOR_PAINTER_BOTS
+    ) {
+      const bot = await gameCtrl.createBotForGame({
+        game,
+        isEstimator: false,
+        gameApplicationUserId: gameApplication.userId,
+        db,
+      });
+
+      if (!bot) {
+        await game.destroy();
+
+        return false;
+      }
+    }
+
+    const gameUsers = [
+      {
+        userId: gameApplication.userId,
+        gameId,
+        isEstimator: false,
+      },
+    ];
+
+    if (gameApplicationSub) {
+      gameUsers.push({
+        userId: gameApplicationSub.userId,
+        gameId,
+        is_estimator: false,
+      });
+    }
+
+    await db.GameUser.bulkCreate(gameUsers);
+
+    // TODO websocket
+    // var message = wsResponses.message('gameFound', {gameId: game.id});
+    // GameApplication.message(gameApplication.id, message)
+    // if (gameApplicationSub) GameApplication.message(gameApplicationSub.id, message)
+
+    await gameApplication.destroy();
+
+    gameApplications.splice(index, 1);
+
+    index -= 1;
+
+    if (gameApplicationSub) {
+      await gameApplicationSub.destroy();
+
+      const subIndex = gameApplications.indexOf((item) => {
+        return item.id === gameApplicationSub.id;
+      });
+
+      gameApplications.splice(subIndex, 1);
+    }
+
+    return true;
+    /* eslint-enable no-param-reassign */
+  }
+
+  async gameApplicationExpired({ gameApplication, gameApplications, index }) {
+    /* eslint-disable no-param-reassign */
+    await gameApplication.destroy();
+
+    gameApplications.splice(index, 1);
+    index -= 1;
+
+    // TODO websocket
+    // GameApplication.message(
+    //   gameApplication.id,wsResponses.message('gameApplicationExpired')
+    // );
+    /* eslint-enable no-param-reassign */
+  }
+
   async gameApplicationIteration({ gameApplications, index = 0, db }) {
     /* eslint-disable no-param-reassign */
     const gameApplication = gameApplications[index];
@@ -15,113 +138,15 @@ export default class ApplicationConnect {
     gameApplication.residueTime -= gameConfig.GAME_APPLICATION_CRONTAB_TIMEOUT;
 
     if (gameApplication.residueTime < 0) {
-      await gameApplication.destroy();
-
-      gameApplications.splice(index, 1);
-      index -= 1;
-
-      // TODO websocket
-      // GameApplication.message(
-      //   gameApplication.id,wsResponses.message('gameApplicationExpired')
-      // );
-
-      return this.gameApplicationIteration({ gameApplications, index: index + 1, db });
-    }
-
-    await gameApplication.save();
-
-    if (!gameApplication.isEstimator) {
-      const gameApplicationSub = this.findOpponentForPlayer(gameApplication, gameApplications);
-
-      if (
-        !gameApplicationSub &&
-        gameApplication.residueTime > gameConfig.RESIDUE_TIME_FOR_PAINTER_BOTS
-      ) {
-        return this.gameApplicationIteration({ gameApplications, index: index + 1, db });
-      }
-
-      const game = await gameCtrl.create();
-      const gameId = game.id;
-
-      if (
-        !gameApplicationSub &&
-        gameApplication.residueTime <= gameConfig.RESIDUE_TIME_FOR_PAINTER_BOTS
-      ) {
-        const bot = await gameCtrl.createBotForGame({
-          game,
-          isEstimator: false,
-          gameApplicationUserId: gameApplication.userId,
-          db,
-        });
-
-        if (!bot) {
-          await game.destroy();
-        }
-
-        return this.gameApplicationIteration({ gameApplications, index: index + 1, db });
-      }
-
-      const gameUsers = [
-        {
-          userId: gameApplication.userId,
-          gameId,
-          isEstimator: false,
-        },
-      ];
-
-      if (gameApplicationSub) {
-        gameUsers.push({
-          userId: gameApplicationSub.userId,
-          gameId,
-          is_estimator: false,
-        });
-      }
-
-      await db.GameUser.bulkCreate(gameUsers);
-
-      // TODO websocket
-      // var message = wsResponses.message('gameFound', {gameId: game.id});
-      // GameApplication.message(gameApplication.id, message)
-      // if (gameApplicationSub) GameApplication.message(gameApplicationSub.id, message)
-
-      await gameApplication.destroy();
-
-      gameApplications.splice(index, 1);
-
-      index -= 1;
-
-      if (gameApplicationSub) {
-        await gameApplicationSub.destroy();
-
-        const subIndex = gameApplications.indexOf((item) => {
-          return item.id === gameApplicationSub.id;
-        });
-
-        gameApplications.splice(subIndex, 1);
-      }
+      this.gameApplicationExpired({ gameApplication, gameApplications, index });
     } else {
-      const gameWithMinEstimators = await gameCtrl.findWithMinEstimators({
-        finderUserId: gameApplication.userId,
-        db,
-      });
+      await gameApplication.save();
 
-      if (!gameWithMinEstimators) {
-        return this.gameApplicationIteration({ gameApplications, index: index + 1, db });
-      }
+      const specialIterationFn = !gameApplication.isEstimator ?
+      this.iterationForPlayer :
+      this.iterationForEstimator;
 
-      await db.GameUser.create({
-        userId: gameApplication.user,
-        gameId: gameWithMinEstimators.id,
-        is_estimator: true,
-      });
-
-      // TODO websocket
-      // GameApplication.message(
-      //   gameApplication.id,
-      //   wsResponses.message('gameFound', {gameId: game.id})
-      // )
-
-      gameApplication.destroy();
+      await specialIterationFn({ gameApplication, gameApplications, index, db });
     }
 
     return this.gameApplicationIteration({ gameApplications, index: index + 1, db });
